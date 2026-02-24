e1da2213b20acafe37eeec698dfea98b0fbf28be
Largen <lvovalex09@gmail.com>
2026-02-24 23:25:34 +0300

    app update

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ if __name__ == '__main__':
         main()
     except Exception:
         logger.exception("Критическая ошибка при запуске приложения")
-        raise # Только тесты
+        raise # Только тесты!!
